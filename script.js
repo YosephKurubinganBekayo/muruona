6b23adeda90135b182cd6c38b72e2060261884b0
YosephKurubinganBekayo <102840619+YosephKurubinganBekayo@users.noreply.github.com>
2022-05-01 00:42:09 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuTogle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menuTogle.addEventListener('click', function () {
-  nav.classList.toggle('slide')
-});
